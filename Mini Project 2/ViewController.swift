8f8e14472cd6feb059df4c181c011b281028b586
Stef <steffiyaegel@yahoo.com>
2022-06-09 14:02:10 -0400

    Initial Commit

---
+++ Mini Project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mini Project 2
+//
+//  Created by Kelsey Lagana on 6/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
